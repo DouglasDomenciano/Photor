7983cb2def4c4b4a3adc849cf99f8658c681bdf7
Douglas Domenciano <douglas.domenciano@bpfidelidade.com.br>
2019-04-12 12:02:31 -0300

    file forgeted

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-
+import React from 'react'
+import { Provider } from "react-redux";
 import {AppRegistry} from 'react-native';
 import Navigator from './src/Navigator'
 import {name as appName} from './app.json';
+import storeConfig from './src/store/storeConfig'
 
-AppRegistry.registerComponent(appName, () => Navigator);
+const store = storeConfig()
+const Redux = () => (
+    <Provider store={store}>
+        <Navigator />
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => Redux);
