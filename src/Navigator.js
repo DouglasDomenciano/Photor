524b07b81da825dd53ca14491e79d3eed3382222
Douglas Domenciano <douglas.domenciano@bpfidelidade.com.br>
2019-04-11 21:43:13 -0300

    Login and Register screens Add

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation'
+import { createBottomTabNavigator, createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Feed from './screens/Feed'
 import AddPhoto from './screens/AddPhoto'
 import Profile from './screens/Profile'
+import Login from './screens/Login'
+import Register from './screens/Register'
+
+const authRouter = createSwitchNavigator({
+    Login: { screen: Login, navigationOptions: { title: 'Signin'}},
+    Register: { screen: Register, navigationOptions: { title: 'Register'}},
+}, {
+    initialRouteName: 'Login'
+})
+const loginOrProfileRouter = createSwitchNavigator({
+    Profile: Profile,
+    Auth: authRouter
+}, {
+    initialRouteName: 'Profile'
+})
 
 const MenuRoutes = { 
     Feed: {
@@ -25,7 +40,7 @@ const MenuRoutes = {
     },
     Profile: {
         name: 'Profile',
-        screen: Profile,
+        screen: loginOrProfileRouter,
         navigationOptions: {
             title: 'Profile',
             tabBarIcon: ({ tintColor }) => <Icon name='user' size={30} color={tintColor} />
